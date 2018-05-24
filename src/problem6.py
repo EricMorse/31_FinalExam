81d41ca24dfe0970bd4f00bc14301f3e51ee65c1
EricMorse <morsee@rose-hulman.edu>
2018-05-24 14:35:47 -0400

    problem6 finished (space correction)

--- src/problem6.py
+++ src/problem6.py
@@ -88,7 +88,7 @@ And this one for n=14:
     #   (and then change the Xs back to spaces).
     # ------------------------------------------------------------------
     for i in range(n):
-        for j in range(n - i):
+        for j in range(n - i - 1):
             print(' ', end='')
         for j in range(i + 1):
             print((j + 1) % 10, end='')
